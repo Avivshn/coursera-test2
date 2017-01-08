45cec96ae30ed4e76b1a6e3567a8d692aa6b3a66
Avivshn <avivshn@gmail.com>
2017-01-08 20:11:48 +0200

    brander

--- BranderLogo/js/js.js
+++ BranderLogo/js/js.js
@@ -3,3 +3,16 @@ $(function(){
         $('#uploadme').click();
     });
 });
+
+
+// $(window).on("load, resize", function() {
+//     var viewportWidth = $(window).width();
+//     if (viewportWidth < 467) {
+//             $("div#uploadfake").removeClass("view view-portfolio");
+//     }
+// });
+
+
+// // $('div#uploadfake').removeAttr('id');
+
+
